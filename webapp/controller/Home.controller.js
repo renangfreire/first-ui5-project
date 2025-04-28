c3f4369ad7646744ab76070dfd7e441460ed8fde
Renan Freire <renan.freire@lab2dev.com>
2025-04-28 20:37:13 +0000

    feat: adicionando JSONModel e Aggregation Binding

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,11 +1,34 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast" 
-], (Controller, MessageToast) => {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], (Controller, MessageToast, JSONModel) => {
     "use strict";
 
     return Controller.extend("studies.firstui5project.controller.Home", {
         onInit() {
+            const data = {
+                "produtos": [
+                    { "nome": "Arroz", "quantidade": 5 },
+                    { "nome": "Feijão", "quantidade": 10 },
+                    { "nome": "Macarrão", "quantidade": 8 },
+                    { "nome": "Óleo de Cozinha", "quantidade": 3 },
+                    { "nome": "Leite", "quantidade": 7 },
+                    { "nome": "Café", "quantidade": 4 },
+                    { "nome": "Açúcar", "quantidade": 6 },
+                    { "nome": "Sal", "quantidade": 2 },
+                    { "nome": "Farinha de Trigo", "quantidade": 9 },
+                    { "nome": "Sabonete", "quantidade": 11 },
+                    { "nome": "Detergente", "quantidade": 5 },
+                    { "nome": "Papel Higiênico", "quantidade": 12 },
+                    { "nome": "Shampoo", "quantidade": 7 },
+                    { "nome": "Condicionador", "quantidade": 6 },
+                    { "nome": "Creme Dental", "quantidade": 8 }
+                  ]
+              }
+              
+            const oModel = new JSONModel(data);
+            this.getView().setModel(oModel, "produtosModel");
         },
         onItemPress: function (oEvent) {
             const oItem = oEvent.getParameter('listItem'); // Retorna o elemento clicado (nesse exemplo: o item)
