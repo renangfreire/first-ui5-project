67fe85e57ef314f73a42c1ca71f45c2a06a2d8a0
Renan Freire <renan.freire@lab2dev.com>
2025-04-28 19:55:01 +0000

    feat: criando message toast e evento de clique na lista

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,10 +1,16 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast" 
+], (Controller, MessageToast) => {
     "use strict";
 
     return Controller.extend("studies.firstui5project.controller.Home", {
         onInit() {
+        },
+        onItemPress: function (oEvent) {
+            const oItem = oEvent.getParameter('listItem'); // Retorna o elemento clicado (nesse exemplo: o item)
+            
+            MessageToast.show(`O item clicado '${oItem.getTitle()}' possui ${oItem.getCounter()} itens`);
         }
     });
 });
