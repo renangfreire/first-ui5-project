dab9d800f728dc0af40b0d0e6fb6db7e5dc039f3
Renan Freire <renan.freire@lab2dev.com>
2025-07-10 18:46:59 +0000

    feat: add search field (filter), add sort and grouping in home view

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -2,8 +2,11 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
-    "studies/firstui5project/model/models"
-], (Controller, MessageToast, JSONModel, models) => {
+    "studies/firstui5project/model/models",
+    "sap/m/GroupHeaderListItem",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], (Controller, MessageToast, JSONModel, models, GroupHeaderListItem) => {
     "use strict";
 
     return Controller.extend("studies.firstui5project.controller.Home", {
@@ -17,6 +20,29 @@ sap.ui.define([
             const oItem = oEvent.getParameter('listItem'); // Retorna o elemento clicado (nesse exemplo: o item)
             
             MessageToast.show(`O item clicado '${oItem.getTitle()}' possui ${oItem.getCounter()} itens`);
+        },
+      
+        criarCabecalhoGrupo: function(oGroup){
+          return new GroupHeaderListItem({
+            title: oGroup.key
+          });
+        },
+        
+        onFiltrarProdutos: function (oEvent) {
+          const sQuery = oEvent.getParameter("newValue");
+          const oList = this.byId("listaProdutos");
+          const oBinding = oList.getBinding("items");
+        
+          let aFiltros = [];
+          if (sQuery) {
+            aFiltros.push(new Filter(
+              "nome",
+              FilterOperator.Contains,
+              sQuery
+            ));
+          }
+        
+          oBinding.filter(aFiltros);
         }
     });
 });
