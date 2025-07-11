fe506c0cc9ca5e7fd968cdd38f450c5985202bd4
Renan Freire <renan.freire@lab2dev.com>
2025-07-11 14:47:37 +0000

    feat: access properties by context in onPressItem

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -31,9 +31,11 @@ sap.ui.define([
             this.getView().setModel(oModel, "produtosModel");
         },
         onItemPress: function (oEvent) {
-            const oItem = oEvent.getParameter('listItem'); // Retorna o elemento clicado (nesse exemplo: o item)
-            
-            MessageToast.show(`O item clicado '${oItem.getTitle()}' possui ${oItem.getCounter()} itens`);
-        }
+            const oItem = oEvent.getParameter('listItem');
+            const oContext = oItem.getBindingContext("produtosModel");
+            const oData = oContext.getObject();
+          
+            MessageToast.show(`O item clicado '${oData.nome}' possui ${oData.quantidade} unidades`);
+          }
     });
 });
